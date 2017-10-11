f8117a4a84cf452db59c5cdc29dda5c959c7da76
Taylor Caton <taylorcaton@gmail.com>
2017-10-11 19:11:08 -0400

    Added notes functionality

--- public/js/app.js
+++ public/js/app.js
@@ -10,8 +10,7 @@ $(document).ready(function() {
       url: "api/saveArticle",
       data: { id: id }
     }).done(function(msg) {
-      
-      $("#"+id)
+      $("#" + id)
         .text("Saved!")
         .removeClass("btn-outline-dark")
         .addClass("btn-success disabled");
@@ -34,61 +33,102 @@ $(document).ready(function() {
     });
   });
 
-  $('#scrapeButton').on('click', function(event) {
-      event.preventDefault();
+  $("#scrapeButton").on("click", function(event) {
+    event.preventDefault();
 
-      console.log('Time to scrape NEW articles!');
+    console.log("Time to scrape NEW articles!");
 
-      $.ajax({
-          method: 'GET',
-          url: 'api/scrape',
-      }).done(function(numArticles){
-          swal(`Scraped ${numArticles} new articles!`)
-          .then(function(){
-            window.location = "/";
-          })
-      })
-  })
+    $.ajax({
+      method: "GET",
+      url: "api/scrape"
+    }).done(function(numArticles) {
+      swal(`Scraped ${numArticles} new articles!`).then(function() {
+        window.location = "/";
+      });
+    });
+  });
 
-  $('#view-notes').on('click', function(event){
-      event.preventDefault();
-      let id = $(this).attr('data-id');
-      console.log(`Viewing the notes for id: ${id}`);
+  $("#view-notes").on("click", function(event) {
+    event.preventDefault();
+    let id = $(this).attr("data-id");
+    console.log(`Viewing the notes for id: ${id}`);
 
-      $.ajax({
-          method: 'GET',
-          url: 'api/getNotes',
-          data: id,
-      }).done(function(notes){
-          let div;
-          notes.forEach(function(note){
-            div += $(`
-            <div class="card">
-          
-                  <div class="card-block">
-                    <div class='card-body'>
-                    
-                       <p class='card-text'>
-                        ${note.text}
-                       <button class='btn btn-danger float-right delete-note' data-id=${note._id} >X</button>
-                       </p> 
-                       
-                    </div>
-                  </div>
-    
-            </div>  
-          `)
-          })
-          swal({
-            title: 'Notes for: ',
-            html: div,
-            input: 'text',
-            inputPlaceholder: 'New Note',
-            confirmButtonText: 'Submit Note',
-            showCancelButton: true,
-          }).then({
+    $.ajax({
+      method: "POST",
+      url: "api/getNotes",
+      data: { id: id }
+    }).done(function(notes) {
+      let div;
+      if (notes) {
+        notes.forEach(function(note) {
+          div += `
+                <div class="card">
               
-          })
+                      <div class="card-block">
+                        <div class='card-body'>
+                        
+                           <p class='card-text'>
+                            ${note.text}
+                           <button class='btn btn-danger float-right delete-note' data-id=${note._id} id='${note._id}'>X</button>
+                           </p> 
+                           
+                        </div>
+                      </div>
+        
+                </div>  
+              `;
+        });
+      } else {
+        div += `<div>
+            Be the first to comment!
+          </div>`;
+      }
+
+      swal({
+        title: "Notes",
+        html: div,
+        input: "text",
+        inputPlaceholder: "New Note",
+        confirmButtonText: "Submit Note",
+        showCancelButton: true
       })
-  })
+        .catch(swal.noop)
+        .then(function(note) {
+          if (note) {
+            $.ajax({
+              method: "POST",
+              url: "api/saveNote",
+              data: { text: note, articleID: id }
+            }).done(function(msg) {
+              console.log(`After note msg: ${msg}`);
+              swal({
+                title: "Note has been saved!"
+              }).then(function() {
+                window.location = "/savedArticles";
+              });
+            });
+          }
+        });
+    });
+  });
+});
+
+$("body").on("click", ".delete-note", function(event) {
+  console.log("Deleting A Note");
+
+  event.preventDefault();
+
+  let noteID = $(this).attr("data-id");
+
+  $.ajax({
+    method: "POST",
+    url: "api/deleteNote",
+    data: { id: noteID }
+  }).done(function(msg) {
+    console.log(msg);
+    $("#" + noteID)
+      .parent()
+      .parent()
+      .remove();
+  });
 });
--- server.js
+++ server.js
@@ -89,6 +89,40 @@ app.post("/api/unsaveArticle", function(req, res) {
   );
 });
 
+app.post("/api/saveNote", function(req, res) {
+  console.log(req.body);
+  let note = new Note(req.body);
+  note.save((err, doc) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(doc);
+    }
+  });
+});
+
+app.post("/api/getNotes", function(req, res) {
+  console.log(req.body);
+  Note.find({articleID:req.body.id}, (err, docs) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(docs);
+    }
+  });
+});
+
+app.post("/api/deleteNote", function(req, res) {
+    console.log(req.body.id);
+    Note.findByIdAndRemove(req.body.id, (err, docs) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(docs);
+      }
+    });
+  });
+
 // A GET request to scrape the echojs website
 app.get("/api/scrape", function(req, res) {
   var getData = require("./clt-scraper.js");
@@ -134,66 +168,6 @@ app.get("/api/scrape", function(req, res) {
         }
       });
     });
-  });
-});
-
-// This will get the articles we scraped from the mongoDB
-app.get("/articles", function(req, res) {
-  // Grab every doc in the Articles array
-  Article.find({}, function(error, doc) {
-    // Log any errors
-    if (error) {
-      console.log(error);
-    } else {
-      // Or send the doc to the browser as a json object
-      res.json(doc);
-    }
-  });
-});
-
-// Grab an article by it's ObjectId
-app.get("/articles/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  Article.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
-    .populate("note")
-    // now, execute our query
-    .exec(function(error, doc) {
-      // Log any errors
-      if (error) {
-        console.log(error);
-      } else {
-        // Otherwise, send the doc to the browser as a json object
-        res.json(doc);
-      }
-    });
-});
-
-// Create a new note or replace an existing note
-app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
-  var newNote = new Note(req.body);
-
-  // And save the new note the db
-  newNote.save(function(error, doc) {
-    // Log any errors
-    if (error) {
-      console.log(error);
-    } else {
-      // Otherwise
-      // Use the article id to find and update it's note
-      Article.findOneAndUpdate({ _id: req.params.id }, { note: doc._id })
-        // Execute the above query
-        .exec(function(err, doc) {
-          // Log any errors
-          if (err) {
-            console.log(err);
-          } else {
-            // Or send the document to the browser
-            res.send(doc);
-          }
-        });
-    }
   });
 });
 
